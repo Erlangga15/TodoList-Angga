f333434cb494efefff579e45f44296ca2103a838
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-02-22 15:01:59 +0700

    add redux state and alpha version

---
+++ webapp/src/store.js
@@ -0,0 +1,32 @@
+const {
+  createAction,
+  createReducer,
+  configureStore,
+} = require('@reduxjs/toolkit');
+const { initialState, add, done, undone, loadTasks } = require('./reducer');
+const thunkMiddleware = require('redux-thunk');
+
+const addAction = createAction('add');
+const doneAction = createAction('done');
+const undoneAction = createAction('undone');
+const loadTasksAction = createAction('loadTasks');
+
+const todoReducer = createReducer(initialState, {
+  [addAction]: add,
+  [doneAction]: done,
+  [undoneAction]: undone,
+  [loadTasksAction]: loadTasks,
+});
+
+const store$ = configureStore({
+  reducer: todoReducer,
+  middleware: [thunkMiddleware.default],
+});
+
+module.exports = {
+  store$,
+  addAction,
+  doneAction,
+  undoneAction,
+  loadTasksAction,
+};
