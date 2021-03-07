5b1b751a5730c7dac33371bfe74d92f2fe55bc06
Erlangga15 <55235710+Erlangga15@users.noreply.github.com>
2021-03-07 13:00:02 +0700

    change to production

--- web/src/config.js
+++ web/src/config.js
@@ -1,2 +1,2 @@
 exports.SENTRY_DSN='https://3aec8903f0d2467eac335512a7ed9cac@o526520.ingest.sentry.io/5663005';
-exports.TODO_SERVICE_BASEURL = 'http://localhost:8000';
+exports.TODO_SERVICE_BASEURL = 'https://todolist-webapp-angga.herokuapp.com';
